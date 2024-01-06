23135556daaf60b0e763cbd9b9e95eb3d9f60870
Anna-Lara Buchner <67783991+anna-larabuchner@users.noreply.github.com>
2024-01-06 11:39:06 +0100

    trying to embed yt video

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 window.onload = function () {
 
+
+  // Load the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+      height: '360',
+      width: '640',
+      videoId: 'dQw4w9WgXcQ'
+    });
+  }
+
+
   const ctaButton = document.getElementById('cta-button--sticky');
   const scrollFunc = () => {
     // Get the current scroll value
